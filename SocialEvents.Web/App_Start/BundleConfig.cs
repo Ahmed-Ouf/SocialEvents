6591ee432cd074a139a56e8a6094cc6a9d90385a
abdelazizaufa <abdelazizaufa@ABDELAZIZAUFA>
2020-02-18 11:08:33 +0300

    add bootstrap 4 new desing

--- SocialEvents.Web/App_Start/BundleConfig.cs
+++ SocialEvents.Web/App_Start/BundleConfig.cs
@@ -13,7 +13,8 @@ namespace SocialEvents.Web.App_Start
                 "~/Scripts/jquery.unobtrusive-ajax.js",
                 "~/Scripts/dataTable/moment-with-locales.js",
                 "~/Scripts/dataTable/jquery.dataTables.min.js",
-                "~/Scripts/dataTable/dataTables.bootstrap.min.js",
+                // "~/Scripts/dataTable/dataTables.bootstrap.min.js",
+                "~/Theme/js/dataTables.bootstrap4.min.js",
                 "~/Scripts/dataTable/datetime-moment.js",
                 "~/Scripts/dataTable/dataTables.rowGroup.min.js",
                 "~/Scripts/dataTable/dataTables.buttons.min.js",
@@ -30,29 +31,37 @@ namespace SocialEvents.Web.App_Start
 
 
             bundles.Add(new ScriptBundle("~/bootstrap/js").Include(
-                "~/js/bootstrap.js",
+                //   "~/js/bootstrap.js",
+                "~/Theme/js/bootstrap.bundle.min.js",
+                "~/Theme/js/font-awesome/all.min.js",
                 "~/Scripts/select2.js",
+                "~/Theme/assets/demo/chart-area-demo.js",
+                "~/Theme/assets/demo/chart-bar-demo.js",
+                "~/Theme/assets/demo/chart-pie-demo.js",
+               // "~/Theme/assets/demo/datatables-demo.js",
                 "~/js/site.js"));
             //------------color pickup-----------
             //bundles.Add(new ScriptBundle("~/spectrum/js").Include("~/Scripts/spectrum.js"));
 
 
             bundles.Add(new StyleBundle("~/bootstrap/css").Include(
-                "~/css/bootstrap.css",
-                "~/Content/dataTable/dataTables.bootstrap.min.css",
+                //"~/css/bootstrap.css",
+                //"~/Content/dataTable/dataTables.bootstrap.min.css",
+                "~/Theme/css/dataTables.bootstrap4.min.css",
                 "~/Content/dataTable/responsive.bootstrap.min.css",
                 "~/Content/dataTable/buttons.dataTables.min.css",
                 "~/Content/dataTable/select.dataTables.min.css",
                 "~/Content/dataTable/rowGroup.dataTables.min.css",
                 "~/Content/css/select2.css",
                 "~/Content/Chart.css",
+                "~/Theme/css/styles.css",
                 "~/css/site.css"));
 
 
             //------------color pickup-----------
             //bundles.Add(new StyleBundle("~/spectrum/css").Include("~/Content/spectrum.css"));
 
-            BundleTable.EnableOptimizations = true;
+            //BundleTable.EnableOptimizations = true;
         }
     }
 }
