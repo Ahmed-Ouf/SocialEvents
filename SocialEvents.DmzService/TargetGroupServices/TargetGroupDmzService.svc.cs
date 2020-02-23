c26a5e349a9c79b2c69eb492230fae8c2a89b7f1
abdelazizaufa <abdelazizaufa@ABDELAZIZAUFA>
2020-02-23 11:33:26 +0300

    Apply targetgroup to mobile api

---
+++ SocialEvents.DmzService/TargetGroupServices/TargetGroupDmzService.svc.cs
@@ -0,0 +1,30 @@
+﻿using SocialEvents.DmzService.TargetGroupWCFServiceRef;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using vm = SocialEvents.ViewModel;
+
+namespace SocialEvents.DmzService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "EventWCFService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select EventWCFService.svc or EventWCFService.svc.cs at the Solution Explorer and start debugging.
+    public class TargetGroupDmzService : ITargetGroupWCFService
+    {
+        private readonly TargetGroupWCFServiceClient _TargetGroupWCFService;
+        public TargetGroupDmzService()
+        {
+            _TargetGroupWCFService = new TargetGroupWCFServiceClient();
+        }
+
+        public List<vm.TargetGroupViewModel> GetTargetGroups()
+        {
+
+            var result = _TargetGroupWCFService.GetTargetGroups();
+
+            return result;
+        }
+    }
+}
---
+++ SocialEvents.MobileApi/Controllers/TargetGroupController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SocialEvents.ViewModel;
+
+namespace SocialEvents.MobileApi.Controllers
+{
+    public class TargetGroupController : ApiController
+    {
+        private readonly TargetGroupDmzServiceRef.ITargetGroupWCFService targetGroupWCFService;
+        public TargetGroupController()
+        {
+            targetGroupWCFService = new TargetGroupDmzServiceRef.TargetGroupWCFServiceClient();
+        }
+        // GET: api/Event
+        public IEnumerable<TargetGroupViewModel> Get()
+        {
+            var result = targetGroupWCFService.GetTargetGroups();
+            return result;
+        }
+
+        // GET: api/Event/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Event
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Event/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Event/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
